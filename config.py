650162daf42659d7b5c0c22b0a403a72b925bae0
VP <vijeta.puthran@gmail.com>
2021-05-09 00:22:50 -0500

    files updated

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 database = "postgres_heroku"
-connect_string = "postgresql+psycopg2://postgres:pgadmin@localhost/project"
+connect_string = "postgresql+psycopg2://rapsyeeaxuvfue:8f005c52901b9f13766f6cf78da8e777e626ae88c6540cd4254fcb53bed2d9df@ec2-107-22-83-3.compute-1.amazonaws.com:5432/d84575gjd8j2q1"
 
 #database = "postgres_local"
 #connect_string = "postgresql+psycopg2://postgres:pgadmin@localhost/project"
