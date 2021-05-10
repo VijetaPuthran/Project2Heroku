79f9009311f9953d57e5b258ac66e1fd80821c1a
VP <vijeta.puthran@gmail.com>
2021-05-09 23:20:09 -0500

    app.py file updated

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from config import database, connect_string
 # relational database class with our data retrieval functions
 from project import Asylum_Seekers
 from flask import Flask, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
-#################################################
-# Flask Setup
-#################################################
-app = Flask(__name__)
 #################################################
 # Database Setup
 #################################################   
 data = Asylum_Seekers()
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
 #################################################
 # Flask Routes
 #################################################
@@ -19,16 +19,15 @@ data = Asylum_Seekers()
 def home():
     """List all available api routes."""
     return (
-        f"<h4>Available Routes:</h4>"
-        f'<a href="/api/asylumseekers">Asylum Seekers</a><br/>'       
+        f"<h4>Available Routes:</h4>"       
         f'<a href="/api/demographics">Demographics</a><br/>' 
         f'<a href="/api/timeseries">Time Series</a><br/>'  
         f'<a href="/"><h4>Back</h4></a><br/>' 
     )       
 
-@app.route("/api/asylumseekers")
-def asy():
-    return jsonify(data.asy_seekers_info())
+# @app.route("/api/asylumseekers")
+# def asy():
+#     return jsonify(data.asy_seekers_info())
 
 @app.route("/api/demographics")
 def demo():
