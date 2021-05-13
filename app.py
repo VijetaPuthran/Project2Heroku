6f755ae667393c9cfc899ecd2ce39385790cf6eb
VP <vijeta.puthran@gmail.com>
2021-05-13 01:47:59 -0500

    files updated

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-#CORS
-from flask import Flask, jsonify, render_template
-#from flask_cors import CORS, cross_origin
-
-
 # dependencies
-# from config import database, connect_string
+from config import database, connect_string
 # relational database class with our data retrieval functions
 from project import Asylum_Seekers
-
+from flask import Flask, jsonify, render_template
 #################################################
 # Database Setup
 #################################################   
@@ -16,21 +11,18 @@ data = Asylum_Seekers()
 # Flask Setup
 #################################################
 app = Flask(__name__)
-#cors = CORS(app)
-#app.config['CORS_HEADERS'] = 'Content-Type'
+
 #################################################
 # Flask Routes
 #################################################
 @app.route("/")
-#@cross_origin()
 def home():
     """List all available api routes."""
     return (
         f"<h4>Available Routes:</h4>"       
         f'<a href="/api/demographics">Demographics</a><br/>' 
-        f'<a href="/api/timeseries">Time Series</a><br/>' 
-        f'<a href="/api/geomap">Geo Map</a><br/>'
-        f'<a href="" onclick="window.history.back();"><h4>Back</h4></a><br/>' 
+        f'<a href="/api/timeseries">Time Series</a><br/>'  
+        f'<a href="/"><h4>Back</h4></a><br/>' 
     )       
 
 # @app.route("/api/asylumseekers")
@@ -38,15 +30,12 @@ def home():
 #     return jsonify(data.asy_seekers_info())
 
 @app.route("/api/demographics")
-#@cross_origin()
 def demo():
     return jsonify(data.demographics())
 
 @app.route("/api/timeseries")
-#@cross_origin()
 def time():
     return jsonify(data.time_series_info())
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-database = "postgres"
-connect_string = "postgres+psycopg2://fqfuqpapgjtkyp:1a278494cd2a3a3d37450aeebd61647daaaca973211086f8ac5fa796927e1bc0@ec2-3-209-176-42.compute-1.amazonaws.com:5432/dft5efisbglob5"
+database = "postgres_heroku"
+connect_string = "postgresql+psycopg2://rapsyeeaxuvfue:8f005c52901b9f13766f6cf78da8e777e626ae88c6540cd4254fcb53bed2d9df@ec2-107-22-83-3.compute-1.amazonaws.com:5432/d84575gjd8j2q1"
 
-
-
-
+#database = "postgres_local"
+#connect_string = "postgresql+psycopg2://postgres:pgadmin@localhost/project"
 
 
